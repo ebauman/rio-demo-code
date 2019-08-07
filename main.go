b8139d3994804ea30cb95b8ab745854498b4b514
Eamon Bauman <eamon@eamonbauman.com>
2019-08-07 12:19:40 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I am rio:v1")
+	fmt.Fprintln(w, "Hi there, I am rio:v2")
 }
 
 func main() {
