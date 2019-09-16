7a620f5805a24a7d1b682f94753c6d02ef2ea8af
Izaac Zavaleta <izaac@rancher.com>
2019-09-16 14:35:36 -0700

    update code

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I am rio:v3")
+	fmt.Fprintln(w, "Hi there, I am rio:v4")
 }
 
 func main() {
